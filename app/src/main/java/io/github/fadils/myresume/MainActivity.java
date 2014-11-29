25a070376af96d8f673e1286b5b5679ded70b209
Fadil Sutomo <fsutomo@gmail.com>
2014-11-29 19:01:30 +0700

    Add background picture

---
+++ app/src/main/java/io/github/fadils/myresume/MainActivity.java
@@ -0,0 +1,24 @@
+package io.github.fadils.myresume;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NoBoringActionBarActivity.class));
+            }
+        });
+    }
+
+
+}
